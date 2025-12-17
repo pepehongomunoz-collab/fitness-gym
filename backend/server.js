af448563fc67b3a7db3ed9400813f3cc6613264e
Nicolas Muñoz <pepehongomunoz@gmail.com>
2025-12-16 21:16:22 -0300

    allow all origins

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://pepehongomunoz-collab.github.io'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: '*', // Allow all origins to unblock requests
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept']
 }));
 app.options('*', cors()); // Enable preflight for all routes
 
